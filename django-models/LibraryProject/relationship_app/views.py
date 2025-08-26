365bf182c9a7606d8040d3e52087dce2fdecd0e7
Lynn Singoei <lynnsingoei@gmail.com>
2025-08-27 00:22:00 +0300

    Final checkups

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth import login
+from django.contrib.auth import login,logout
 
 # Create your views here.
 def books_list(request):
@@ -22,7 +22,25 @@ class LibraryDetailView(DetailView):
         context['books'] = Book.objects.filter(library=self.object)
         return context
     
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'relationship_app/register.html'
+
+def register(request):
+    form_class=UserCreationForm
+
+    if request.method=='POST':
+        form=form_class(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request,user)
+            
+    else:
+        form=form_class()
+    return render(request,'relationship_app/register.html',{'form':form})
+
+
+def login_view(request):
+    return render(request, 'relationship_app/login.html')
+
+
+def logout_view(request):
+    return render(request, 'relationship_app/logout.html')
+
