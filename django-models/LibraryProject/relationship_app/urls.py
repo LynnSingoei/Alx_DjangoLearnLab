80284830f5625b18fca5872774c6a679a27015f8
Lynn Singoei <lynnsingoei@gmail.com>
2025-08-27 00:31:09 +0300

    "minor changes"

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,15 @@
 from .views import books_list
 from django.urls import path
 from .views import LibraryDetailView
-from .views import SignUpView
-from django.contrib.auth import views as auth_views
+from .views import register,login_view,logout_view
+
 
 urlpatterns = [
     path('books/', books_list, name='books_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('signup/', register(), name='register'),
+    path('login/', login_view(), name='login'),
+    path('logout/', logout_view(), name='logout'),
     
 
 ]
