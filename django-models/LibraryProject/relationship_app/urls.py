c034356eed4fb7386cf402d8add1896627173428
Lynn Singoei <lynnsingoei@gmail.com>
2025-08-27 00:39:45 +0300

    "minor changes"

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,7 +9,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('books/', books_list, name='books_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('signup/', register(), name='register'),
+    path('signup/', register, name='register'),
     path(
         'login/',
         LoginView.as_view(template_name='relationship_app/login.html'),
