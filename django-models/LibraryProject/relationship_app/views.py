bc99b87510bf6c5c4a29ff0def32c56797c681b8
Lynn Singoei <lynnsingoei@gmail.com>
2025-08-26 15:03:22 +0300

    "added the templates "

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,13 +4,11 @@ from .models import Book
 # Create your views here.
 def books_list(request):
     books=Book.objects.all()
-    output=""
-    for book in books:
-        output += f"Title: {book.title}, Author: {book.author.name}<br>"
-    return HttpResponse(output)
+    return render(request, "relationship_app/book_list.html", {"books": books})
 
 class Library_view(DetailView):
     model=Library
+    template_name="relationship_app/library_detail.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
