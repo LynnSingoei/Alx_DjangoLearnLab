1f6ef4ae400fee3a246d40be4f8e425bca5ed6c4
Lynn Singoei <lynnsingoei@gmail.com>
2025-08-27 21:21:33 +0300

    "made some changes"

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,9 +24,9 @@ urlpatterns = [
     path('admin/dashboard/', views.admin_view,name='admin_view'),
     path('librarian/dashboard/', views.librarian_view,name='librarian_view'),
     path('member/dashboard/', views.member_view,name='member_view'),
-     path('edit/', views.edit_book, name='edit_book'),
-    path('delete/', views.delete_book, name='delete_book'),
-    path('add/', views.add_book, name='add_book'),
+     path('edit_book/', views.edit_book, name='edit_book'),
+    path('delete_book/', views.delete_book, name='delete_book'),
+    path('add_book/', views.add_book, name='add_book'),
 
 
     
